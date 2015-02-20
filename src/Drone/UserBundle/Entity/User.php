7cfa71c917c785c66f7db41b2fd79f7db3072508
Julien Calixte <jcalixte@isep.fr>
2015-02-20 21:49:03 +0100

    Ajout de bootstrap. Ajout des templates FOSUserBundle à surcharger. Surcharge du formulaire d'inscription. Ajout d'un trigger sur l'handler au clique sur la carte

--- src/Drone/UserBundle/Entity/User.php
+++ src/Drone/UserBundle/Entity/User.php
@@ -6,11 +6,13 @@ namespace Drone\UserBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Drone\MapBundle\Entity\Map as Map;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ * @Vich\Uploadable
  */
 class User extends BaseUser
 {
@@ -46,6 +48,27 @@ class User extends BaseUser
      * @ORM\Column(name="country", type="string", length=50, nullable=false)
      */
     private $country;
+
+    /**
+     * @Vich\UploadableField(mapping="user_image", fileNameProperty="userImage")
+     *
+     * @var File $imageFile
+     */
+    protected $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="user_image")
+     *
+     * @var string $userImage
+     */
+    protected $userImage;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime $updatedAt
+     */
+    protected $updatedAt;
 
     public function __construct()
     {
@@ -94,5 +117,41 @@ class User extends BaseUser
     public function getMaps()
     {
         return $this->maps;
+    }
+
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param string $userImage
+     */
+    public function setUserImage($userImage)
+    {
+        $this->userImage = $userImage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserImage()
+    {
+        return $this->userImage;
     }
 }
---
+++ src/Drone/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drone\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // add your custom field
+        $builder->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+          	->add('imageFile', 'vich_image', array(
+						'required'      => false,
+						'allow_delete'  => true,
+						'download_link' => true,
+          		))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'drone_user_registration';
+    }
+}
+
