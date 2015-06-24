32ce0392bd46b93d306aca6fb237dad734258534
Julien Calixte <juliencalixte@hotmail.fr>
2015-06-25 00:30:59 +0200

    Changement insignifiant

--- src/Drone/MapBundle/Controller/BeeyeController.php
+++ src/Drone/MapBundle/Controller/BeeyeController.php
@@ -9,21 +9,20 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class BeeyeController extends FOSRestController
 {
-	public function getDroneAction($id){
+	public function getDroneAction($id) {
 		$drone = $this->getDoctrine()
 		->getRepository('DroneMapBundle:Drone')
 		->find($id);
 
 		if (!$drone) {
 			throw $this->createNotFoundException(
-				'Aucun drone trouvé pour cet identifiant : '.$id
+				'Aucun drone trouvé pour cet identifiant : ' . $id
 			);
 		}
 		return $drone;
 	}
 
-	public function getDroneTestAction($id)
-	{
+	public function getDroneTestAction($id) {
 		return true;
 	}
 }
